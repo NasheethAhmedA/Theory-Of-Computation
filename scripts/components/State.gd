7d87b63cf719784abf045e7794b77f44546177bd
Nasheeth Ahmed A <nasheethahmeda04@gmail.com>
2024-07-05 17:13:04 +0530

    data flow state
    signal connected and data is passed both ways between state and state_form

--- scripts/components/State.gd
+++ scripts/components/State.gd
@@ -1,15 +1,17 @@
 class_name State extends Node2D
 
-# All the Exports
-@export_category("State Data")
-@export var Final: bool = false
-@export var State_Name: String = "q"
 
 # Children Nodes
 @onready var final_state_bg = $final_state_bg
 @onready var label = $Label
 @onready var area_2d = $Area2D
 @onready var state_form = $State_Form
+
+# All the Exports
+@export_category("State Data")
+@export var Final: bool = false
+@export var State_Name: String = "q"
+		
 
 var Selected: bool = false
 var dragged: bool = false
@@ -18,35 +20,29 @@ func _input(event):
 	if Selected:
 		if event.is_action_pressed("make_final"):
 			Final = not Final
-		elif event.is_action_pressed("info"):
+			state_form.final = Final
+			set_state_data()
+		elif event.is_action_pressed("Rclick"):
 			state_form.visible = not state_form.visible
-		if event.is_action_pressed("drag"):
+		if event.is_action_pressed("Lclick"):
 			dragged = true
-		if event.is_action_released("drag"):
+		if event.is_action_released("Lclick"):
 			dragged = false
 			
-		
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_state_data()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	Set_State_Data()
 	if dragged:
 		global_position = lerp(global_position, get_viewport().get_mouse_position(), 25*_delta)
 
-
-# Set the State data from the export values
-func Set_State_Data():
-	State_Name = State_Name.substr(0, 3)
+func set_state_data():
+	label.text = State_Name.substr(0, 3)
 	final_state_bg.visible = Final
-	label.text = State_Name
-
-
 
 func _on_area_2d_mouse_entered():
 	Selected = true
@@ -54,3 +50,10 @@ func _on_area_2d_mouse_entered():
 
 func _on_area_2d_mouse_exited():
 	Selected = false
+
+
+func _on_state_form_on_data_change(New_Name, New_Final):
+	State_Name = New_Name
+	Final = New_Final
+	set_state_data()
+	
--- scripts/ui/state_form.gd
+++ scripts/ui/state_form.gd
@@ -1,4 +1,31 @@
 extends Control
 
+signal on_data_change(New_Name, New_Final)
+
+@onready var name_edit = $Panel/NameEdit
+@onready var is_final = $Panel/isFinal
+
+var Name := ""
+var final := false:
+	set (value):
+		final = value
+		is_final.button_pressed = value
+
 func _on_exit_btn_pressed():
 	visible = false
+
+
+func _on_set_btn_pressed():
+	on_data_change.emit(Name, final)
+	visible =false
+
+
+func _on_name_edit_text(new_text):
+	Name = new_text
+
+
+
+
+func _on_is_final_toggled(toggled_on):
+	final = toggled_on
+
